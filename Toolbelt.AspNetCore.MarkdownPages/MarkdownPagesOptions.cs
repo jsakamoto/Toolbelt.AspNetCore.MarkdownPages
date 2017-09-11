a2e12b4a1ff4864a6b9773c617ddfdac9bb50298
jsakamoto <junichi.jp@gmail.com>
2017-09-11 22:28:45 +0900

    Improve: Make default settings of meta tags and css links to replaceable cleanly.

--- Toolbelt.AspNetCore.MarkdownPages/MarkdownPagesOptions.cs
+++ Toolbelt.AspNetCore.MarkdownPages/MarkdownPagesOptions.cs
@@ -6,13 +6,13 @@ namespace Toolbelt.AspNetCore.MarkdownPages
 {
     public class MarkdownPagesOptions
     {
-        public List<string> MetaTags { get; } = new List<string> {
+        public List<string> MetaTags { get; set; } = new List<string> {
             "<meta charset=\"utf-8\" />",
             "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=Edge\" />",
             "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />"
         };
 
-        public List<string> CssLinks { get; } = new List<string>();
+        public List<string> CssLinks { get; set; } = new List<string>();
 
         public bool EnableAutoLink { get; set; } = true;
     }
